a5e7f81d76d1029e959ba2b6774c674b1e64b792
mohamedsaad <mahameds19999@gmail.com>
2020-04-08 22:57:46 +0200

    Edit the routes & add the singleServicePage

--- src/globals/routes.js
+++ src/globals/routes.js
@@ -1,9 +1,9 @@
 import HomePage from "modules/static-pages/home/components/page";
 import AboutPage from "modules/static-pages/about/components/page";
+import SingleServicePage from "modules/teamServices/components/singleServicePage";
 
 import EventsListPage from "modules/events/components/eventsListPage";
 import SingleEventPage from "modules/events/components/singleEventPage";
-import EventFormPage from "modules/events/components/eventFormPage";
 
 import BlogListPage from "modules/blog/components/blogListPage";
 import SingleBlogPage from "modules/blog/components/singleBlogPage";
@@ -13,9 +13,9 @@ import ContactPage from "modules/contact/components/page";
 export default [
   { path: "/", component: HomePage, label: "Home" },
   { path: "/about-us", component: AboutPage, label: "About us" },
+  { path: "/services/:id", component: SingleServicePage },
   { path: "/events", component: EventsListPage, label: "Events" },
   { path: "/events/:id", component: SingleEventPage },
-  { path: "/events/:id/register", component: EventFormPage },
   { path: "/blog", component: BlogListPage, label: "Blog" },
   { path: "/blog/:id/:title", component: SingleBlogPage },
   { path: "/contact-us", component: ContactPage, label: "Contact us" },
--- src/modules/events/components/eventFormPage/index.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from "react";
-
-export default class EventFormPage extends Component {
-  render() {
-    return <h1> Event Form Page is ready ! </h1>;
-  }
-}
