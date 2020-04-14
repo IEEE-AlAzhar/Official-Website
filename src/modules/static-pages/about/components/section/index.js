9ee46e8abf4bb0f49411bb39aeacbe63da1ea83d
LeenaAhmed <Leena3792@gmail.com>
2020-04-14 06:52:59 +0200

    resolve

--- src/modules/static-pages/about/components/section/index.js
+++ src/modules/static-pages/about/components/section/index.js
@@ -8,22 +8,22 @@ import {Link} from 'react-router-dom';
 export default class About extends Component {
   render() {
      return (
-      <section  className={styles["about-home-section"]}>
+      <section  className={styles["about-home_section"]}>
          <div  className="container">
-           <h2 className={styles["about-home-Heading"]}> About Us</h2>
+           <h2 className={styles["about-home_Heading"]}> About Us</h2>
            <div className="row">
              <div className="col-lg-6"> 
                    <AboutVideo/>
              </div>
             <div className="col-lg-6">
-              <div className={styles['about-Info']}>
-                <p className={styles["about-home-lead"]} >
+              <div className={styles['about-home_Info']}>
+                <p className={styles["about-home_lead"]} >
                     Was founded in 2012 its dependent to "IEEE EGYPT SECTION" which exists in all universities in Egypt.Our Mission: Raising the Name of Al-Azhar University. Providing the required scientific and practical conditions to prepare 
                     a successful engineer from the students ,
                       so they become able to compete in the labor market.
                 </p>
-                <Link to='/about-us'>
-                    <button className={styles["about-home-button"]}> Read More </button>
+                <Link to='/about-us' className={styles["about-home_button"]}>
+                    Read More 
                 </Link>
               </div>
             </div>
--- src/modules/static-pages/home/components/intro/index.js
+++ src/modules/static-pages/home/components/intro/index.js
@@ -5,11 +5,11 @@ export default   class Intro extends Component {
     render(){
         const {title ,subtitle } =this.props
         return ( 
-            <header  className={styles["intro-header"]}>
-                <div  className={styles["intro-overlay"]}></div>
+            <header  className={styles["intro-header_section"]}>
+                <div  className={styles["intro-header_overlay"]}></div>
                  <div className="container">
-                    <h1 className={styles["intro-header-heading"]}>{title}</h1>
-                    <p className={styles["intro-header-subtitle"]}> {subtitle}</p>
+                    <h1 className={styles["intro-header_heading"]}>{title}</h1>
+                    <p className={styles["intro-header_subtitle"]}> {subtitle}</p>
                 </div>
           </header>
         );
