42c1f14e694995cd8e30c8008641f615f018def7
mohamedsaad <mahameds19999@gmail.com>
2020-04-17 13:33:20 +0200

    Edits

--- src/modules/contact/components/page/index.js
+++ src/modules/contact/components/page/index.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-import {Helmet} from "react-helmet";
-import Layout from 'shared/layout';
-import Icon from '../icon/index'
-import Form from '../form/index'
+import { Helmet } from "react-helmet";
+import Layout from "shared/layout";
+import Icon from "../icon/index";
+import Form from "../form/index";
 import styles from "./style.module.css";
 
 class ContactPage extends Component {
@@ -12,10 +12,19 @@ class ContactPage extends Component {
         <Helmet>
           <title>Contact Us</title>
         </Helmet>
-        <div className='text-center modal-dialog-centered px-3 py-2' style={{backgroundColor: '#0a111a'}}> {/* var(--layout-scondary) is too bad at dark mode! */}
-          <section className={`row container px-1 py-4 mx-auto rounded`} style={{backgroundColor: 'var(--layout-primary)',fontFamily: 'var(--heading-font-family)'}}>
+        <div
+          className="text-center modal-dialog-centered px-3 py-2"
+          style={{ backgroundColor: "#0a111a" }}
+        >
+          <section
+            className={`row container px-1 py-4 mx-auto rounded`}
+            style={{
+              backgroundColor: "var(--layout-primary)",
+              fontFamily: "var(--heading-font-family)",
+            }}
+          >
             <Icon />
-            <div className={`mr-3 ${styles['vertical-line']}`} />
+            <div className={`mr-3 ${styles["vertical-line"]}`} />
             <Form />
           </section>
         </div>
