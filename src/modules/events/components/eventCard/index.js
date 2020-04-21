44093d6feafe257025fc3791cca223d02a852f24
Mu'taz Alaa <mutazalaasalah@gmail.com>
2020-04-21 11:07:56 +0200

    eventslist

--- src/modules/events/components/eventCard/index.js
+++ src/modules/events/components/eventCard/index.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 
 export default class EventCard extends Component {
   render() {
+    console.log(this.props.data);
     return <div></div>;
   }
 }
--- src/modules/events/components/eventsListPage/index.js
+++ src/modules/events/components/eventsListPage/index.js
@@ -1,12 +1,52 @@
 import React, { Component } from "react";
-import UpComingEvents from "./components/upComingEvents";
 import Layout from "shared/layout/index";
+import styles from "./style.module.css";
+import { getEvents } from "../../services/events.service";
 export default class EventsListPage extends Component {
+  state = {
+    eventsList: null,
+  };
+  componentDidMount() {
+    getEvents().then((res) => {
+      this.setState({
+        eventsList: res.data,
+      });
+      console.log(res.data);
+    });
+  }
   render() {
     return (
       <>
         <Layout>
-          <UpComingEvents />
+          {this.state.eventsList
+            ? this.state.eventsList.map((event) => {
+                if (event.status === "upcoming") {
+                  return (
+                    <div key={event.id}>
+                      <div className={styles[`upcoming-events`]}>
+                        <img src={event.cover} alt="event cover" />
+                        <h1>{event.title}</h1>
+                        <p>{event.description}</p>
+                        <p>{event.endDate}</p>
+                        <p>{event.location}</p>
+                      </div>
+                    </div>
+                  );
+                } else {
+                  return (
+                    <div key={event.id}>
+                      <section>
+                        <img src={event.cover} alt="event cover" />
+                        <h1>{event.title}</h1>
+                        <p>{event.description}</p>
+                        <p>{event.endDate}</p>
+                        <p>{event.location}</p>
+                      </section>
+                    </div>
+                  );
+                }
+              })
+            : null}
         </Layout>
       </>
     );
