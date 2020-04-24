c2ce7367ce8a3bf70ba42065e723c224444e5ad4
mohamedsaad <mahameds19999@gmail.com>
2020-04-24 16:06:38 +0200

    solve conflicts & edits

--- src/modules/events/components/eventCard/index.js
+++ src/modules/events/components/eventCard/index.js
@@ -6,62 +6,71 @@ import {
   faMapMarkerAlt,
   faCalendarCheck,
   faCalendarTimes,
-  faClock
 } from "@fortawesome/free-solid-svg-icons";
 
 class EventCard extends Component {
   render() {
-   
-    const {event } = this.props ;
-     return (
-           <section className={styles["event-card_content"]}>
-              <figure className={styles["events-card_cover"]} key={event}>
-                  <Link to={'/events/' + event.id}>
-                      <img  
-                        alt={`event ${event.title}`}
-                        src={event.cover}
-                        key={event.id} 
-                        />
-                  </Link>
-                  <div className={styles['activ-upcoming_event']}>
-                    {event.status === "upcoming"?<span>
-                     <FontAwesomeIcon icon={faClock}
-                        className={styles["activ-upcoming-event_icon"]}/></span>: ''}
-                  </div>
-                  <figcaption >
-                      <h3>
-                        <Link to={'/events/' + event.id} className={styles["event-card_title"]}>
-                          {event.title}
-                          </Link>
-                      </h3>
-                      <p className={styles["event-card_description"]}>
-                          {event.description}
-                      </p>
-                  </figcaption>
-              </figure>
-              <section className={styles["event-card_SubDescription"]}>
-                 <div className={styles["event-card-SubDescription_content"]} >
-                    <div className={styles["event-card-SubDescription_info"]}>
-                         <FontAwesomeIcon icon={faCalendarCheck } 
-                            className={styles["event-card_icon"]} />        
-                          <span> {event.startDate}</span>
-                     </div>
-                        <div className={styles["event-card-SubDescription_info"]}>
-                          {event.location.length ? (<>
-                            <FontAwesomeIcon  icon={faMapMarkerAlt}
-                              className={styles["event-card_icon"]} /> 
-                            <span>{ event.location}</span>
-                               </>
-                          ) : ( <> 
-                             <FontAwesomeIcon  icon={ faCalendarTimes}
-                             className={styles["event-card_icon"]} /> 
-                            <span>{event.endDate}</span> </> ) }
-                      </div>
-                   </div>
-                </section>
-            </section>
-         )
-    }
+    const { event } = this.props;
+    return (
+      <section className={styles["event-card_content"]}>
+        <figure className={styles["events-card_cover"]} key={event}>
+          <Link to={"/events/" + event.id}>
+            <img
+              alt={`event ${event.title}`}
+              src={event.cover}
+              key={event.id}
+            />
+          </Link>
+          <div className={styles["active-upcoming_event"]}>
+            {event.status === "upcoming" ? <span>Upcoming</span> : ""}
+          </div>
+          <figcaption>
+            <h3>
+              <Link
+                to={"/events/" + event.id}
+                className={styles["event-card_title"]}
+              >
+                {event.title}
+              </Link>
+            </h3>
+            <p className={styles["event-card_description"]}>
+              {event.description}
+            </p>
+          </figcaption>
+        </figure>
+        <section className={styles["event-card_SubDescription"]}>
+          <div className={styles["event-card-SubDescription_content"]}>
+            <div className={styles["event-card-SubDescription_info"]}>
+              <FontAwesomeIcon
+                icon={faCalendarCheck}
+                className={styles["event-card_icon"]}
+              />
+              <span> {event.startDate}</span>
+            </div>
+            <div className={styles["event-card-SubDescription_info"]}>
+              {event.location.length ? (
+                <>
+                  <FontAwesomeIcon
+                    icon={faMapMarkerAlt}
+                    className={styles["event-card_icon"]}
+                  />
+                  <span>{event.location}</span>
+                </>
+              ) : (
+                <>
+                  <FontAwesomeIcon
+                    icon={faCalendarTimes}
+                    className={styles["event-card_icon"]}
+                  />
+                  <span>{event.endDate}</span>{" "}
+                </>
+              )}
+            </div>
+          </div>
+        </section>
+      </section>
+    );
+  }
 }
 
 export default EventCard;
