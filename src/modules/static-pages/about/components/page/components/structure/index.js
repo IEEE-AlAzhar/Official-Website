3a0b3d4082dd65a2758b51e4e1c912fc18c31f33
mutazalaa <mutazalaasalah@gmail.com>
2020-04-28 03:28:07 +0200

    remove intersection observer function

--- src/modules/static-pages/about/components/page/components/structure/index.js
+++ src/modules/static-pages/about/components/page/components/structure/index.js
@@ -7,26 +7,24 @@ import IEEEStr1200 from "assets/images/structure-1200.jpg";
 import IEEEStr2500 from "assets/images/structure-2500.jpg";
 
 class Structure extends Component {
-  componentDidMount() {
-    const observer = new IntersectionObserver(([entry]) => {
-      if (entry.isIntersecting === true && window.innerWidth <= 767) {
-        this.IEEEStrImg.current.innerHTML = `<img src=${IEEEStr650} class = "${styles.IEEE_str_img__structureImg}" alt="Tree shows the structure of the team" />`;
-      } else if (entry.isIntersecting === true && window.innerWidth >= 1200) {
-        this.IEEEStrImg.current.innerHTML = `<img src=${IEEEStr1200} class ="${styles.IEEE_str_img__structureImg}" alt="Tree shows the structure of the team"  />`;
-      } else if (entry.isIntersecting === true && window.innerWidth >= 2500) {
-        this.IEEEStrImg.current.innerHTML = `<img src=${IEEEStr2500} class = "${styles.IEEE_str_img__structureImg}" alt="Tree shows the structure of the team" />`;
-      }
-    });
-    observer.observe(this.IEEEStrImg.current);
-  }
   render() {
-    this.IEEEStrImg = React.createRef();
     return (
       <>
         <section className={styles.IEEE_str}>
           <div className={styles.IEEE_Str_content}>
             <h1 className={styles.IEEE_Str_content__heading}>Our Structure</h1>
-            <div className={styles.IEEE_str_img} ref={this.IEEEStrImg}></div>
+            <div className={styles.IEEE_str_img}>
+              <img
+                src={
+                  window.innerWidth <= 767
+                    ? IEEEStr650
+                    : window.innerWidth >= 1200
+                    ? IEEEStr1200
+                    : IEEEStr2500
+                }
+                alt="IEEE Alazhar -structure-team"
+              />
+            </div>
           </div>
         </section>
       </>
