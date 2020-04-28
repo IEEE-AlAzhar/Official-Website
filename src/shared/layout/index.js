784a52e796cc60b651db82a335beedbc91f64b43
Mu'taz Alaa <52619217+moatazalaa14@users.noreply.github.com>
2020-04-28 02:43:25 +0200

    Update index.js

--- src/shared/layout/index.js
+++ src/shared/layout/index.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import Header from "shared/header";
 import Footer from "shared/footer";
+
+import "./style.css";
+
+
 export default class Layout extends Component {
   render() {
     let { children } = this.props;
