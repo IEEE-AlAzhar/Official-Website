0911becc3b7a3cd9969da776a37fe6461fae3bb2
mutazalaa <mutazalaasalah@gmail.com>
2020-04-29 01:33:08 +0200

    edit upcoming events and make it in the same row

--- src/modules/events/components/eventsListPage/index.js
+++ src/modules/events/components/eventsListPage/index.js
@@ -19,46 +19,51 @@ export default class EventsListPage extends Component {
     return (
       <>
         <Layout>
-          <div className={styles[`events-list`]}>
-            <div className="container">
-              <h1 className={styles[`events-heading`]}>Our Events</h1>
-              <h2 className={styles[`events-heading`]}>Upcoming Events</h2>
-              <div className="row">
-                {this.state.eventsList ? (
-                  this.state.eventsList.map((event) => {
-                    if (event.status === "upcoming") {
-                      return (
-                        <div className="lg-col-12 md-col-12 sm-col-12 mt-4">
-                          <EventCard event={event} key={event.id} />
-                        </div>
-                      );
-                    } else {
-                      return null;
-                    }
-                  })
-                ) : (
-                  <span>Loading ...</span>
-                )}
-                <h2 className={styles[`past-heading`]}>Past Events</h2>
-                <div className="row">
-                  {this.state.eventsList ? (
-                    this.state.eventsList.map((event) => {
-                      if (event.status !== "upcoming") {
-                        return (
-                          <div className="col-lg-4 col-md-6 col-sm-12 mt-4">
-                            <EventCard event={event} key={event.id} />
-                          </div>
-                        );
-                      } else {
-                        return null;
-                      }
-                    })
-                  ) : (
-                    <span>Loading ...</span>
-                  )}
-                </div>
-              </div>
-            </div>
+          <h1 className={styles[`events-heading`]}>Our Events</h1>
+
+          <div className="container">
+            <h2 className={styles[`events-heading`]}>Upcoming Events</h2>
+            <section className="row ">
+              {this.state.eventsList ? (
+                this.state.eventsList.map((event) => {
+                  if (event.status === "upcoming") {
+                    return (
+                      <div
+                        className="col-lg-4 col-md-6 col-sm-12  mt-4"
+                        key={event.id}
+                      >
+                        <EventCard event={event} />
+                      </div>
+                    );
+                  } else {
+                    return null;
+                  }
+                })
+              ) : (
+                <span>Loading ...</span>
+              )}
+            </section>
+            <h2 className={styles[`past-heading`]}>Past Events</h2>
+            <section className="row">
+              {this.state.eventsList ? (
+                this.state.eventsList.map((event) => {
+                  if (event.status !== "upcoming") {
+                    return (
+                      <div
+                        className="col-lg-4 col-md-6 col-sm-12 mt-4 "
+                        key={event.id}
+                      >
+                        <EventCard event={event} />
+                      </div>
+                    );
+                  } else {
+                    return null;
+                  }
+                })
+              ) : (
+                <span>Loading ...</span>
+              )}
+            </section>
           </div>
         </Layout>
       </>
