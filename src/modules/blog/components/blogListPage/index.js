002e39a4f8fc9f8c31e1c55faa7af72aea6f1e52
Abud Zohair <abud.zohair@gmail.com>
2020-04-29 13:33:14 +0200

    Separate Fetching data

--- src/modules/blog/components/blogListPage/index.js
+++ src/modules/blog/components/blogListPage/index.js
@@ -1,38 +1,20 @@
 import React, { Component } from "react";
 import BlogCard from "./../blogCard/index";
-import axios from "axios";
 import { Helmet } from "react-helmet";
-import Pagination from "../../../../globals/pagination";
 import styles from "./style.module.css";
+import { getBlogs } from "modules/blog/services/blog.service";
 
 export default class BlogListPage extends Component {
   state = {
     blogs: [],
-    currentPage: 1,
-    pageSize: 3,
   };
 
   componentDidMount() {
-    axios
-      .get("./data/blogs.json")
-      .then(({ data: blogs }) => this.setState({ blogs }));
+    getBlogs().then(({ data: blogs }) => this.setState({ blogs }));
   }
 
-  blogsRender = (currentPage, blogs, pageSize) => {
-    return blogs.slice(
-      currentPage * pageSize - pageSize,
-      currentPage * pageSize
-    );
-  };
-
-  handlePaginationClick = (page) => {
-    let { currentPage } = this.state;
-    currentPage = page > 0 ? page : currentPage + page || currentPage + 1;
-    this.setState({ currentPage });
-  };
   render() {
-    const { currentPage, pageSize, blogs } = this.state;
-    const blogsToRender = this.blogsRender(currentPage, blogs, pageSize);
+    const { blogs } = this.state;
     return (
       <>
         <Helmet>
@@ -42,18 +24,12 @@ export default class BlogListPage extends Component {
         <div className="container">
           <div className="row">
             <section className={`col-lg-8 ${styles.blogs__list}`}>
-              {blogsToRender.map((blog) => (
+              {blogs.map((blog) => (
                 <BlogCard key={blog.id} data={blog} />
               ))}
             </section>
           </div>
         </div>
-        <Pagination
-          blogsCount={this.state.blogs.length}
-          pageSize={this.state.pageSize}
-          currentPage={this.state.currentPage}
-          onPaginationClick={this.handlePaginationClick}
-        />
       </>
     );
   }
