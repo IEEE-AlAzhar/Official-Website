55171ea955e3ee162ca49e4534f5c3f60a68ee1a
IEEE Al-Azhar <61624936+IEEE-AlAzhar@users.noreply.github.com>
2022-11-05 17:09:08 +0200

    read api base url from env

--- src/globals/core.service.js
+++ src/globals/core.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://ieee-website-dashboard.herokuapp.com/api",
+  baseURL: process.env.REACT_APP_BASE_URL,
 });
 
 export default class CrudService {
